6a8af1621a589e0f69b6309336f5e25167a5df01
Philipp Arras <parras@mpa-garching.mpg.de>
2020-08-18 22:09:05 +0200

    Work on main script

--- demo/imaging_with_automatic_weighting.py
+++ demo/imaging_with_automatic_weighting.py
@@ -25,22 +25,25 @@ def main():
     obs = rve.ms2observations(args.ms, 'DATA')[0].restrict_to_stokes_i()
 
     rve.set_epsilon(1/10/obs.max_snr())
-    dst = 0.045*rve.AS2RAD
-    dom = ift.RGSpace((4096, 2048), (dst, dst))
-    sky = ift.SimpleCorrelatedField(dom, 21, (1, 0.1), (5, 1), (1.2, 0.4), (0.2, 0.2), (-2, 0.5)).exp()
+    fov = np.array([3, 1.5])*rve.ARCMIN2RAD
+    npix = np.array([4096, 2048])/4
+    dom = ift.RGSpace(npix, fov/npix)
+    logsky = ift.SimpleCorrelatedField(dom, 21, (1, 0.1), (5, 1), (1.2, 0.4), (0.2, 0.2), (-2, 0.5))
+    sky = logsky.exp()
     inserter = rve.PointInserter(sky.target, np.array([[0, 0], [0.7, -0.34]])*rve.AS2RAD)
     points = ift.InverseGammaOperator(inserter.domain, alpha=0.5, q=0.2/dom.scalar_dvol).ducktape('points')
     sky = sky + inserter @ points
     sky = rve.vla_beam(dom, np.mean(obs.freq)) @ sky
-    plotter = rve.Plotter('png', 'plots')
-    plotter.add('logsky', sky.log())
-
-    ic = ift.AbsDeltaEnergyController(0.5, 3, 2000)
-    minimizer = ift.NewtonCG(ift.GradientNormController(name='newton', iteration_limit=5))
-
     alpha = 1
     invcovop = ift.InverseGammaOperator(obs.vis.domain, alpha, 1/obs.weight).reciprocal().ducktape('invcov')
+    plotter = rve.Plotter('png', 'plots')
+    plotter.add('logsky', logsky)
+    plotter.add('power spectrum logsky', logsky.power_spectrum)
     plotter.add_uvscatter('inverse covariance', invcovop, obs)
+
+    ic = ift.AbsDeltaEnergyController(0.5, 3, 100, name='Sampling')
+    minimizer = ift.NewtonCG(ift.GradientNormController(name='newton', iteration_limit=5))
+
     ham = ift.StandardHamiltonian(rve.ImagingLikelihoodVariableCovariance(obs, sky, invcovop), ic)
 
     pos = ift.MultiField.union([0.1*ift.from_random(ham.domain), ift.full(invcovop.domain, norm.ppf(invgamma.cdf(1, alpha)))])
