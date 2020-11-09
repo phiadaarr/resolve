9efaf40d1ab9c5533bfc4934cab9882c61cf3eaf
Philipp Arras <parras@mpa-garching.mpg.de>
2020-11-09 10:04:00 +0100

    Move bench

--- demo/mf_imaging.py
+++ demo/mf_imaging.py
@@ -131,49 +131,5 @@ def main():
     # FIXME Figure out how to do automatic weighting for mf
 
 
-def bench_cumsum_helper(npix, nfreq):
-    from time import time
-    a = np.zeros((2, nfreq, npix, npix))
-    t0 = time()
-    np.cumsum(a[0], axis=0)
-    t1 = time()
-    np.cumsum(a[1], axis=0)
-    t2 = time()
-
-    s0 = time()
-    b = np.transpose(a, (0, 2, 3, 1))
-    b = np.ascontiguousarray(b)
-    s1 = time()
-    np.cumsum(b[0], axis=-1)
-    s2 = time()
-    np.cumsum(b[1], axis=-1)
-    s3 = time()
-    b = np.transpose(a, (0, 3, 1, 2))
-    b = np.ascontiguousarray(b)
-    s4 = time()
-
-    return t2-t0, s4-s0
-
-
-def bench_cumsum():
-    xs = [64, 70, 80, 93, 100, 128, 150, 189, 200, 256, 500, 512, 995, 999,
-          1000, 1022, 1024, 2000, 2048]
-    ys0 = []
-    ys1 = []
-    for ii, npix in enumerate(xs):
-        print(ii, len(xs))
-        nai, transp = bench_cumsum_helper(npix, 40)
-        ys0.append(nai)
-        ys1.append(transp)
-    plt.plot(xs, ys0, label='Naive')
-    plt.plot(xs, ys1, label='With transpose')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.legend()
-    plt.savefig('bench_cumsum.png')
-    print('Wrote bench_cumsum.png')
-
-
 if __name__ == '__main__':
-    bench_cumsum()
     main()
