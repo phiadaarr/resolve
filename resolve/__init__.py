3ef735f84da7732544d858f04530da423a6f03ff
Philipp Arras <parras@mpa-garching.mpg.de>
2020-11-21 11:56:19 +0100

    Cosmetics

--- resolve/__init__.py
+++ resolve/__init__.py
@@ -4,19 +4,20 @@ from .global_config import *
 from .likelihood import (CalibrationLikelihood, ImagingCalibrationLikelihood,
                          ImagingLikelihood,
                          ImagingLikelihoodVariableCovariance,
-                         MfImagingLikelihoodVariableCovariance,
-                         MfImagingLikelihood)
+                         MfImagingLikelihood,
+                         MfImagingLikelihoodVariableCovariance)
 from .minimization import Minimization, MinimizationState, simple_minimize
 from .mpi import onlymaster
 from .ms_import import ms2observations, ms_n_spectral_windows
 from .multi_frequency.irg_space import IRGSpace
 from .multi_frequency.operators import (IntWProcessInitialConditions,
-                                        WienerIntegrations, MfWeightingInterpolation)
+                                        MfWeightingInterpolation,
+                                        WienerIntegrations)
 from .observation import Observation, tmin_tmax, unique_antennas, unique_times
 from .plotter import MfPlotter, Plotter
 from .points import PointInserter
 from .primary_beam import vla_beam
-from .response import StokesIResponse, MfResponse
+from .response import MfResponse, StokesIResponse
 from .simple_operators import AddEmptyDimension
 from .util import (Reshaper, divide_where_possible, my_assert,
                    my_assert_isinstance, my_asserteq)
