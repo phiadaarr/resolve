6fea16fc91420b0e0b2a5688c65493f6b9158f48
Simon Ding <ding@mpa-garching.mpg.de>
2021-04-12 21:26:06 +0200

    hard coded to use double precision

--- resolve/response.py
+++ resolve/response.py
@@ -119,6 +119,7 @@ class MfResponse(ift.LinearOperator):
         # Make sure that no index is wasted
         my_asserteq(len(set(band_indices)), frequency_domain.size)
         self._r = []
+        # FIXME: Make sure to use double precision despite having data in single precision
         sp = observation.vis.dtype == np.complex64
         mask = observation.mask
         for band_index in np.unique(band_indices):
@@ -129,7 +130,7 @@ class MfResponse(ift.LinearOperator):
                 observation.uvw,
                 observation.freq[sel],
                 mask[0, :, sel].T,
-                sp,
+                False,
                 verbose
             )
             self._r.append((band_index, sel, r))
