c3f1783c6954ba53d56dd1e4d3150bd5e9a360ad
Philipp Arras <c@philipp-arras.de>
2021-12-06 18:44:33 +0100

    Skymodel:

--- resolve/sky_model.py
+++ resolve/sky_model.py
@@ -32,13 +32,13 @@ def single_frequency_sky(cfg_section):
         kwargs1 = {
             "offset_mean": cfg_section.getfloat(f"{pol_lbl} zero mode offset"),
             "offset_std": (cfg_section.getfloat(f"{pol_lbl} zero mode mean"),
-                           cfg_section.getfloat(f"{pol_lbl} zero mode stddev"))
+                           cfg_section.getfloat(f"{pol_lbl} zero mode stddev")),
+            "prefix": f"{pol_lbl} "
         }
         kwargs2 = {kk: _parse_or_none(cfg_section, f"{pol_lbl} space {kk}")
                    for kk in ["fluctuations", "loglogavgslope", "flexibility", "asperity"]}
         op = ift.SimpleCorrelatedField(sdom, **kwargs1, **kwargs2)
         additional[f"logdiffuse stokes{lbl} power spectrum"] = op.power_spectrum
-        op = op @ ift.PrependKey(op.domain, f"{pol_lbl} ").adjoint
         logsky.append(op.ducktape_left(lbl))
     logsky = reduce(add, logsky)
     mfs = MultiFieldStacker((pdom, sdom), 0, pdom.labels)
