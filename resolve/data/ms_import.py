1dcc6e725cc7a24c885d86cf9f810a2fa64565c9
Philipp Arras <c@philipp-arras.de>
2022-01-29 16:53:51 +0100

    Dirty image visualization

--- resolve/data/ms_import.py
+++ resolve/data/ms_import.py
@@ -27,10 +27,11 @@ def _pol_id(ms_path, spectral_window):
 
 
 def ms2observations_all(ms, data_column):
-    res = []
     for spw in range(ms_n_spectral_windows(ms)):
-        res.extend(ms2observations(ms, data_column, True, spw))
-    return list(filter(lambda x: x is not None, res))
+        obs = ms2observations(ms, data_column, True, spw)
+        for oo in obs:
+            if oo is not None:
+                yield oo
 
 
 def ms2observations(ms, data_column, with_calib_info, spectral_window,
