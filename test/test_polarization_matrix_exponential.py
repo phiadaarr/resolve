7011e807c8b52ca31fd25a12a1e9ac49bf2e2f6c
Philipp Arras <c@philipp-arras.de>
2022-02-14 16:06:36 +0100

    PolarizationMatrixExponential: Add tests

---
+++ test/test_polarization_matrix_exponential.py
@@ -0,0 +1,46 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright(C) 2022 Max-Planck-Society, Philipp Arras
+# Author: Philipp Arras
+
+import nifty8 as ift
+import pytest
+
+import resolve as rve
+
+from .common import list2fixture
+
+pmp = pytest.mark.parametrize
+
+pdom = list2fixture([rve.PolarizationSpace("I"),
+                     rve.PolarizationSpace(["I", "Q", "U"]),
+                     rve.PolarizationSpace(["I", "Q", "U", "V"]),
+                     ])
+
+restdom = list2fixture([[ift.UnstructuredDomain(7)],
+                        [ift.RGSpace([2, 3]), rve.IRGSpace([12., 13., 130])]
+                        ])
+
+
+def test_jax_vs_nifty(pdom, restdom):
+    dom = tuple((pdom,)) + tuple(restdom)
+    op0 = rve.polarization_matrix_exponential(dom, False)
+    op1 = rve.polarization_matrix_exponential(dom, True)
+
+    loc = ift.from_random(op0.domain)
+
+    ift.extra.check_operator(op0, loc, ntries=3)
+    ift.extra.check_operator(op1, loc, ntries=3)
+
+    ift.extra.assert_allclose(op0(loc), op1(loc))
