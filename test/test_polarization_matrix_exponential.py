8606545de0bb6fafb70aae95a5b3f171c162d7d6
Philipp Arras <c@philipp-arras.de>
2022-02-17 09:53:33 +0100

    Move tests

--- test/test_polarization_matrix_exponential.py
+++ test/test_polarization_matrix_exponential.py
@@ -44,3 +44,21 @@ def test_jax_vs_nifty(pdom, restdom):
     ift.extra.check_operator(op1, loc, ntries=3)
 
     ift.extra.assert_allclose(op0(loc), op1(loc))
+
+
+@pmp("pol", ("I", ["I", "Q", "U"], ["I", "Q", "U", "V"]))
+def test_polarization(pol):
+    dom = rve.PolarizationSpace(pol), rve.IRGSpace([0]), rve.IRGSpace([0]), ift.RGSpace([10, 20])
+    op = rve.polarization_matrix_exponential(dom, False)
+    pos = ift.from_random(op.domain)
+    ift.extra.check_operator(op, pos, ntries=5)
+    try:
+        op_jax = rve.polarization_matrix_exponential(dom, True)
+
+        assert op.domain is op_jax.domain
+        assert op.target is op_jax.target
+
+        ift.extra.assert_allclose(op(pos), op_jax(pos))
+        ift.extra.check_operator(op_jax, pos, ntries=5)
+    except ImportError:
+        pass
