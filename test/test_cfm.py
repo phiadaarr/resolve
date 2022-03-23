4730739f3ba9baea069d52856492047aaf919dc2
Philipp Arras <c@philipp-arras.de>
2022-03-23 21:19:07 +0100

    Implement cpp version of correlated field model

---
+++ test/test_cfm.py
@@ -0,0 +1,49 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright(C) 2022 Max-Planck-Society, Philipp Arras
+# Author: Philipp Arras
+
+import nifty8 as ift
+
+import resolve as rve
+from .common import setup_function, teardown_function
+import pytest
+
+pmp = pytest.mark.parametrize
+
+
+@pmp("total_N", [0, 1, 3])
+@pmp("prefix", ["", "b"])
+def test_cfm(total_N, prefix):
+    dom0 = ift.RGSpace(10)
+    dom1 = ift.RGSpace(10)
+
+    dofdex = list(range(total_N))
+    args0 = dict(prefix=prefix, total_N=total_N)
+    args1 = dict(target_subdomain=dom0, fluctuations=(1., 1.), flexibility=(2., 2), asperity=(0.1, 0.1), loglogavgslope=(-2, .1), prefix="dom0", dofdex=dofdex)
+    args2 = dict(target_subdomain=dom1, fluctuations=(2., 0.1), flexibility=(1., 2), asperity=(0.2, 0.1), loglogavgslope=(-3, .321), prefix="dom1", dofdex=dofdex)
+    args3 = dict(offset_mean=1.2, offset_std=(1., 0.2), dofdex=dofdex)
+    cfm = ift.CorrelatedFieldMaker(**args0)
+    cfm.add_fluctuations(**args1)
+    cfm.add_fluctuations(**args2)
+    cfm.set_amplitude_total_offset(**args3)
+    op0 = cfm.finalize(0)
+
+    cfm = rve.CorrelatedFieldMaker(**args0)
+    cfm.add_fluctuations(**args1)
+    cfm.add_fluctuations(**args2)
+    cfm.set_amplitude_total_offset(**args3)
+    op1 = cfm.finalize()
+
+    rve.operator_equality(op0, op1)
