Fix in variable covariance with flags

resolve/weighting_model.py
@@ -80,7 +80,7 @@ def log_weighting_model(cfg, obs, sky_domain):
             tmpop = (restructure @ linear_interpolation @ log_weights).scale(-2)
             if oo.is_single_precision():
                 tmpop = DtypeConverter(tmpop.target, np.float64, np.float32) @ tmpop
-            tmpop = ift.Adder(oo.weight.log()) @ tmpop
+            tmpop = ift.Adder(_log_if_not_zero(oo.weight)) @ tmpop
             op.append(tmpop)
         return op, additional
     if cfg["model"] == "independent gamma":
@@ -125,3 +125,9 @@ class _CustomRestructure(ift.LinearOperator):
                 for ii in range(self.target.shape[2]):
                     res[_polfreq_key(pp, ii)] = x[pp, :, ii]
         return ift.makeField(self._tgt(mode), res)
+
+
+def _log_if_not_zero(fld):
+    res = fld.log().val_rw()
+    res[fld.val == 0.] = 0.
+    return ift.makeField(fld.domain, res)